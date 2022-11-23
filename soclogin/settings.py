a84c2d51c71c3ec73685a69d2a5d4343a9b3624d
Morten Noerregaard <76176578+driftingdane@users.noreply.github.com>
2022-11-23 15:38:20 +0100

    Update settings.py

--- soclogin/settings.py
+++ soclogin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'soclogin.urls'
 TEMPLATES = [
 	{
 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        	'DIRS': [BASE_DIR / 'templates'],
+        	'DIRS': [os.path.join(BASE_DIR, 'templates')],
         	'APP_DIRS': True,
 		'OPTIONS': {
 			'context_processors': [
@@ -123,28 +123,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-if DEBUG:
-
-    STATIC_URL = '/static/'
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-    STATICFILES_DIRS = [
-	BASE_DIR / "static",
-    ]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # media folder settings
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = BASE_DIR / 'media'
-
-else:
-    STATIC_URL = '/static/'
-    STATIC_ROOT = BASE_DIR / 'static'
-
-    # media folder settings
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SITE_ID = 2
 LOGIN_REDIRECT_URL = '/'
