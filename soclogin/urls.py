b2bfded9b0172f7e9ae9a51c09b22c6466f6c734
Morten Noerregaard <76176578+driftingdane@users.noreply.github.com>
2022-11-23 15:45:41 +0100

    Update urls.py

--- soclogin/urls.py
+++ soclogin/urls.py
@@ -24,9 +24,3 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-#if settings.DEBUG:
-#    urlpatterns += static(settings.STATIC_URL,
-#                          document_root=settings.STATIC_ROOT)
-#    urlpatterns += static(settings.MEDIA_URL,
-#                          document_root=settings.MEDIA_ROOT)
