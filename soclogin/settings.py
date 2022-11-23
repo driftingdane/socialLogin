2f5c17c4dd5ee80b64b66d0ce58520c4f5a42c75
Morten Noerregaard <76176578+driftingdane@users.noreply.github.com>
2022-11-23 11:52:54 +0100

    Update settings.py

--- soclogin/settings.py
+++ soclogin/settings.py
@@ -124,21 +124,26 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+if DEBUG:
 
-STATIC_URL = '/static/'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Set for production server
-# STATIC_ROOT = BASE_DIR / 'static'
-
-# Set for local development
-STATICFILES_DIRS = [
-	BASE_DIR / 'static',
-]
-
+    STATICFILES_DIRS = [
+	BASE_DIR / "static",
+    ]
 # media folder settings
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
+
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR / 'static'
+
+    # media folder settings
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 SITE_ID = 2
 LOGIN_REDIRECT_URL = '/'
